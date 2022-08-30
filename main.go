d6b943243b2c0e5582e9ba4484d0bedc9632c49e
Satyajit <emailtovamos@gmail.com>
2022-08-30 20:29:37 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+)
+
+const (
+	myToken  = "<MYTOKEN>"
+	myOrg    = "<MYORG>"
+	myBucket = "<MYBUCKET>"
+)
+
+func main() {
+	// Create a new client using an InfluxDB server base URL and an authentication token
+	client := influxdb2.NewClient("http://localhost:8086", myToken)
+
+	// Use blocking write client for writes to desired bucket
+	writeAPI := client.WriteAPIBlocking(myOrg, myBucket)
+
+	//  1.  Create point using full params constructor
+	p := influxdb2.NewPoint("stat",
+		map[string]string{"unit": "temperature"},
+		map[string]interface{}{"avg": 24.5, "max": 45.0},
+		time.Now())
+	// write point immediately
+	writeAPI.WritePoint(context.Background(), p)
+
+	// 2.  Create point using fluent style
+	p = influxdb2.NewPointWithMeasurement("stat").
+		AddTag("unit", "temperature").
+		AddField("avg", 23.2).
+		AddField("max", 45.0).
+		SetTime(time.Now())
+	writeAPI.WritePoint(context.Background(), p)
+
+	// 3.  Or write directly line protocol
+	line := fmt.Sprintf("stat,unit=temperature avg=%f,max=%f", 23.5, 45.0)
+	writeAPI.WriteRecord(context.Background(), line)
+
+	// Get query client
+	queryAPI := client.QueryAPI(myOrg)
+	// Get parser flux query result
+	result, err := queryAPI.Query(context.Background(), `from(bucket:"as")|> range(start: -1h) |> filter(fn: (r) => r._measurement == "stat")`)
+	if err == nil {
+		// Use Next() to iterate over query result lines
+		for result.Next() {
+			// read result
+			fmt.Printf("row: %s\n", result.Record().String())
+		}
+		if result.Err() != nil {
+			fmt.Printf("Query error: %s\n", result.Err().Error())
+		}
+	}
+	// Ensures background processes finishes
+	client.Close()
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# InfluxDB Golang Example
+
+This is a simple example of how to set up influxDB locally and run the golang client to store and query time series data
+
+- Install InfluxDB
+- Run `influxd`
+- Go to `localhost:8086`
+- Sign up, sign in, get your Org, token and create bucket
+- Insert these values in the code
+- `go mod tidy`
+- Run `go run main.go`
